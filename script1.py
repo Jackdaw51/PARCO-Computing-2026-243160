d216a9af78d0f6ff68061ce9c6609fb50ea14473
Jackdaw51 <frabo@Aloy.>
2025-11-22 17:04:44 +0100

    How it should be'

--- script1.py
+++ script1.py
@@ -22,9 +22,12 @@ for mtx in row_size:
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 print(row_size)
-array_of_times = []
 for mtx in row_size:
-    times = []
+    array_of_avgs = []
+    array_of_perms = []
+    array_of_sorts = []
+    array_of_inv_perms = []
+    array_of_total_times = []
     for tn in thread_numbers:
         filename = f"output_my_impl/{tn}_{mtx}.bgn"
         try:
@@ -35,23 +38,41 @@ for mtx in row_size:
             times.append(np.nan)
             continue
 
-        # extract floating number (e.g. "123.45 ms")
-        m = re.search(r'([-+]?\d*\.\d+|\d+)', content)
-        if m:
-            time_ms = float(m.group(0))
-            times.append(time_ms)
+        # extract floating number 
+        # format is 
+        # avg time: XXX.XXXX ms
+        # permutation time: XXX.XXXX ms
+        # sorting time: XXX.XXXX ms
+        # inverse permutation time: XXX.XXXX ms
+
+        #search for strings separated by new lines
+        values = re.findall(r'([-+]?\d*\.\d+|\d+)', content)
+        if values and len(values) >= 4:
+            array_of_avgs.append(float(values[0]))
+            array_of_perms.append(float(values[1]))
+            array_of_sorts.append(float(values[2]))
+            array_of_inv_perms.append(float(values[3]))
+            array_of_total_times.append(float(values[0]) + float(values[1]) + float(values[2]) + float(values[3]))
         else:
             print(f"Warning: No time found in {filename}; inserting NaN")
-            times.append(np.nan)
-
+            array_of_avgs.append(np.nan)
+            array_of_perms.append(np.nan)
+            array_of_sorts.append(np.nan)
+            array_of_inv_perms.append(np.nan)
+            array_of_total_times.append(np.nan)
         print(mtx)
         print(thread_numbers)
-        print(times)
-    plt.scatter(thread_numbers, times, s=100)
+        print(array_of_avgs)
+    plt.scatter(thread_numbers, array_of_avgs, s=100)
+    plt.scatter(thread_numbers, array_of_perms, s=100)
+    plt.scatter(thread_numbers, array_of_sorts, s=100)
+    plt.scatter(thread_numbers, array_of_inv_perms, s=100)
+    plt.scatter(thread_numbers, array_of_total_times, s=100)
     plt.xlabel("Number of Threads")
     plt.ylabel("Time (ms)")
     plt.title("My implementation")
     plt.xticks(thread_numbers)
+    plt.legend(["Average SpMV Time", "Permutation Time", "Sorting Time", "Inverse Permutation Time", "Total Time"])
     plt.grid()
     plt.savefig(f"pics_my_impl/{mtx}/my_impl_times.png", dpi=150, bbox_inches='tight')
     plt.clf()  # Clear figure for next type
