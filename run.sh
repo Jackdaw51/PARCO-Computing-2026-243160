c0dae778a8b6348d525288333ab743d9fa1bff3e
Jackdaw51 <frabo@Aloy.>
2025-11-22 12:14:08 +0100

    changed to main_old

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ for MATRIX in $mtx_arr; do
         for SCHEDULING in FOR STATIC DYNAMIC GUIDED; do
             if [ "$SCHEDULING" != "FOR" ]; then
                 for CSIZE in 1 10 100 1000; do
-                    gcc -o main main.c -fopenmp -DTHREAD_NUMBER=$THREADS -D$SCHEDULING -DCHUNKSIZE=$CSIZE 
+                    gcc -o main main_old.c -fopenmp -DTHREAD_NUMBER=$THREADS -D$SCHEDULING -DCHUNKSIZE=$CSIZE 
                     echo "Running with $THREADS threads and $SCHEDULING scheduling (chunk size: $CSIZE) on matrix $MATRIX"
                     ./main $MATRIX
                 done
                 continue
             fi            
-            gcc -o main main.c -fopenmp -DTHREAD_NUMBER=$THREADS -D$SCHEDULING
+            gcc -o main main_old.c -fopenmp -DTHREAD_NUMBER=$THREADS -D$SCHEDULING
             echo "Running with $THREADS threads and $SCHEDULING scheduling on matrix $MATRIX"
             ./main $MATRIX
         done
